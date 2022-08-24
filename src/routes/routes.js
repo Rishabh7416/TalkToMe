032ae078e9282bb4c7b3c3eed8783ef3271b6790
admin <admin@192.168.1.8>
2022-08-24 23:00:03 +0530

    Navigation Bottom Tabs

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import React from 'react';
 import ChatList from '../screens/chat/chatList';
+import VideoCall from '../screens/video/videoCall';
+import Settings from '../screens/settings/settings';
+import ContactList from '../screens/contacts/contactList';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 const BottomTabs = createBottomTabNavigator();
 
 export default function Routes() {
   return (
     <NavigationContainer>
-        <BottomTabs.Navigator>
-            {/* <BottomTabs.Screen name = "" component={}/> */}
-            <BottomTabs.Screen name = "Chat" component={ChatList}/>
-            {/* <BottomTabs.Screen name = "" component={}/>
-            <BottomTabs.Screen name = "" component={}/> */}
-        </BottomTabs.Navigator>
+      <BottomTabs.Navigator screenOptions={{headerShown: false}}>
+        <BottomTabs.Screen name="Chat" component={ChatList} />
+        <BottomTabs.Screen name="Contacts" component={ContactList} />
+        <BottomTabs.Screen name="Video" component={VideoCall} />
+        <BottomTabs.Screen name="Settings" component={Settings} />
+      </BottomTabs.Navigator>
     </NavigationContainer>
-  )
+  );
 }
